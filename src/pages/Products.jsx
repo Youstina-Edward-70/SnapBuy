80d9368e455622679164cec556529425af332740
Youstina-Edward-70 <https://github.com/Youstina-Edward-70/>
2025-10-04 18:05:37 +0300

    Some Edit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -53,64 +53,66 @@ function ProductsPage() {
                 </p>
             </div>
 
-            <DrawerBar onCategorySelect={setSelectedCategory} />
+            <section className="mx-auto max-w-6xl">
+                <DrawerBar onCategorySelect={setSelectedCategory} />
 
-            <Typography variant="h5" sx={{ mt: 3, mb: 2 }}>
-                {selectedCategory
-                    ? `Products in "${selectedCategory}" category`
-                    : searchQuery
-                        ? `Search results for "${searchQuery}"`
-                        : "All Products"}
-            </Typography>
+                <Typography variant="h5" sx={{ mt: 3, mb: 2 }}>
+                    {selectedCategory
+                        ? `Products in "${selectedCategory}" category`
+                        : searchQuery
+                            ? `Search results for "${searchQuery}"`
+                            : "All Products"}
+                </Typography>
 
 
-            {loading ? (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto max-w-6xl mt-6">
-                    {[...Array(8)].map((_, index) => (
-                        <div key={index} className="border rounded-lg p-4 shadow dark:border-gray-600 dark:bg-gray-700 bg-gray-100">
-                            <Skeleton variant="rectangular" height={180} animation="wave" sx={{ borderRadius: 2, mb: 2 }} />
-                            <Skeleton variant="text" height={30} width="80%" />
-                            <Skeleton variant="text" height={20} width="60%" />
-                            <Skeleton variant="rectangular" height={40} width="100%" sx={{ borderRadius: 2, mt: 2 }} />
+                {loading ? (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+                        {[...Array(8)].map((_, index) => (
+                            <div key={index} className="border rounded-lg p-4 shadow dark:border-gray-600 dark:bg-gray-700 bg-gray-100">
+                                <Skeleton variant="rectangular" height={180} animation="wave" sx={{ borderRadius: 2, mb: 2 }} />
+                                <Skeleton variant="text" height={30} width="80%" />
+                                <Skeleton variant="text" height={20} width="60%" />
+                                <Skeleton variant="rectangular" height={40} width="100%" sx={{ borderRadius: 2, mt: 2 }} />
+                            </div>
+                        ))}
+                    </div>
+                ) : products.length === 0 ? (
+                    <div className="text-center mt-10">
+
+                        <p>No products found.</p>
+                    </div>
+                ) : null}
+
+
+                {/* Products Grid */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    {products.map(product => (
+                        <div key={product.id} className="border rounded-lg p-4 shadow hover:shadow-lg flex flex-col dark:border-gray-600 dark:bg-gray-700 bg-gray-100 transition duration-500">
+                            <img
+                                src={product.thumbnail}
+                                alt={product.title}
+                                className="w-full h-48 object-cover mb-4 rounded"
+                            />
+                            <div className="p-4 flex flex-col flex-grow">
+                                <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
+                                <p className="text-sm text-gray-600 dark:text-gray-400 mb-2">{product.description}</p>
+                                <div className="mt-auto"><div className="text-lg font-bold text-blue-600 dark:text-blue-400">
+                                    {product.price.toLocaleString("en-US", {
+                                        style: "currency",
+                                        currency: "USD",
+                                        maximumFractionDigits: 2,
+                                    })}</div></div>
+                            </div>
+                            <div className="flex flex-col justify-center gap-3 p-4 border-t border-gray-300 dark:border-gray-600 transition duration-500">
+                                <CartButton product={product} className={"px-4 py-2 bg-gray-200 text-gray-900 hover:bg-gray-300 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"} />
+                                <Link to={`${product.id}`} className="px-4 py-2 text-center bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-500">
+                                    Details
+                                </Link>
+                            </div>
                         </div>
                     ))}
                 </div>
-            ) : products.length === 0 ? (
-                <div className="text-center mt-10">
-
-                    <p>No products found.</p>
-                </div>
-            ) : null}
-
-
-            {/* Products Grid */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto max-w-6xl">
-                {products.map(product => (
-                    <div key={product.id} className="border rounded-lg p-4 shadow hover:shadow-lg flex flex-col dark:border-gray-600 dark:bg-gray-700 bg-gray-100 transition duration-500">
-                        <img
-                            src={product.thumbnail}
-                            alt={product.title}
-                            className="w-full h-48 object-cover mb-4 rounded"
-                        />
-                        <div className="p-4 flex flex-col flex-grow">
-                            <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
-                            <p className="text-sm text-gray-600 dark:text-gray-400 mb-2">{product.description}</p>
-                            <div className="mt-auto"><div className="text-lg font-bold text-blue-600 dark:text-blue-400">
-                                {product.price.toLocaleString("en-US", {
-                                    style: "currency",
-                                    currency: "USD",
-                                    maximumFractionDigits: 2,
-                                })}</div></div>
-                        </div>
-                        <div className="flex flex-col justify-center gap-3 p-4 border-t border-gray-300 dark:border-gray-600 transition duration-500">
-                            <CartButton product={product} className={"px-4 py-2 bg-gray-200 text-gray-900 hover:bg-gray-300 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"} />
-                            <Link to={`${product.id}`} className="px-4 py-2 text-center bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-500">
-                                Details
-                            </Link>
-                        </div>
-                    </div>
-                ))}
-            </div>
+            </section>
 
         </div>
     );
