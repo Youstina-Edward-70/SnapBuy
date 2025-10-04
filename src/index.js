162e560670ef117705d538a32fd09e33e933b8b3
Youstina-Edward-70 <https://github.com/Youstina-Edward-70/>
2025-10-04 19:20:24 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// نحدد basename تلقائي حسب البيئة
 let basename = '/';
 if (process.env.REACT_APP_BASENAME) {
-  basename = process.env.REACT_APP_BASENAME; // GitHub Pages
+  basename = process.env.REACT_APP_BASENAME; 
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
